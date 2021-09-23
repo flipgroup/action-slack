0c394e8f74fd9067828176168e35a2ab42c4de77
Peter Mescalchin <peter@flip.com.au>
2021-09-23 16:01:16 +1000

    Swapped out `ncc` for `esbuild` for action build

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 DIRNAME=$(dirname "$0")
 
 
-npm install --no-save @vercel/ncc@^0.31.1
-npx ncc build "$DIRNAME/src/main.js" --license=licenses.txt
-mv "$DIRNAME/dist/index.js" "$DIRNAME/dist/main.js"
+npm install --no-save esbuild@^0.13.1
+npx esbuild "$DIRNAME/src/main.js" \
+  --bundle \
+  --outfile="$DIRNAME/dist/main.js" \
+  --platform=node
